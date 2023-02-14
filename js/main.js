b52ea3ec5791a1d1cdcf2e41eb650a59194371af
mat <matpomianowski@gmail.com>
2023-02-14 23:30:13 +0100

    rozpoczęcie zadania

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let num1 = 2;
+let num2 = 5;
+function liczby(licz1, licz2) {
+	if (licz1 > licz2) {
+		return licz1;
+	}
+	if (licz1 < licz2) {
+		return licz2;
+	}
+}
+console.log(liczby(num1, num2));
